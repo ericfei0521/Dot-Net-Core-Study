12f3789c6646c214b5b98e3a39db383d2624fbb9
ericfei <ericfei@glaibo.com>
2021-04-12 09:54:41 +0800

    step: data type and convert data type

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace dotnetTest
+namespace MyApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+            var transferNum = Convert.ToString(myInt); // Convert int to string
+            Console.WriteLine(transferNum);
+            Console.WriteLine(Convert.ToDouble(myInt));    // Convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble));  // Convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // Convert bool to string
         }
     }
 }
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dot-Net-Core-Study
 The road for learning .net core and C# 
+
+### Basic explained:
+### In Program.cs file : 
+#### Link 1: using System means that we can use classes from the System nampespace.
